c36f9608a35e9d7d209086f561e08a1f4bce98b9
Yancarmxi <yanczi@outlook.com>
2024-11-22 09:16:58 +0100

    Perwsze pruby z session

--- animelista_server/main.js
+++ animelista_server/main.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const session = require('express-session');
 const config = require('./src/config');
+const userRoute = require('./src/database/user');
 
 const app = express();
 const port = config['server']['port'];
 
+app.use(session({
+  secret: config['server']['key'],
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
 app.get('/', (req, res) => {
-  res.send('Hello World! ');
+  //res.send('Hello World! ');
+  if (!req.session.views) {
+    req.session.views = 1;
+  } else {
+      req.session.views++;
+  }
+  res.send(`Odwiedziłeś tę stronę ${req.session.views} razy`);
 })
+
+app.get('/user', userRoute);
 
 app.get('/anime', async (req, res) => {
   const r = await require('./src/database/anime');
--- animelista_server/src/database/user.js
+++ animelista_server/src/database/user.js
@@ -1,6 +1,25 @@
 const loadsql = require('./load');
+const express = require('express');
+const router = express.Router();
 
-const sql = 'SELECT id, user, data_registration, avatar FROM users';
-const result = loadsql(sql);
+router.get('/set', async (req, res) => {
+    try {
+        const sql = 'SELECT user, data_registration, avatar FROM users WHERE id = 1';
+        const result = await loadsql(sql);
 
-module.exports = result;
+        const user = result[0];
+        const imgBlob = user.avatar;
+
+        req.session.user_name = user.name;
+        req.session.user_regdate = user.data_registration;
+        req.session.user_avatar = imgBlob.ToString('Base64');
+
+        res.send('Załadowanie danych powiodło się');
+    }
+    catch(e) {
+        res.send(e);
+        res.status(500);
+    }
+});
+
+module.exports = router;

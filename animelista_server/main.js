b48fc1cba64aaaf8003459a9ef8700cca3befd79
Yancarmxi <yanczi@outlook.com>
2024-11-23 18:31:01 +0100

    Przerabianie sessji na MYSQL. Poprawić serializację danych na STRING JSON

--- animelista_server/main.js
+++ animelista_server/main.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
 const cors = require('cors');
 const config = require('./src/config');
 const userRoute = require('./src/login');
@@ -20,15 +21,41 @@ app.use((req, res, next) => {
   next();
 });
 
+const dbOptions = {
+  host: config['database']['host'],
+  port: config['database']['port'],
+  user: config['database']['user'],
+  password: config['database']['password'],
+  database: config['database']['database']
+};
+
+const sessionStore = new MySQLStore(dbOptions);
+
 app.use(session({
-  secret: config['server']['key'],
-  resave: false,
-  saveUninitialized: true,
+  key: 'animu_lista_cookie_session', // Nazwa ciasteczka sesji
+  secret: config['server']['key'], // Klucz tajny używany do podpisywania ciasteczek
+  store: sessionStore, // Magazyn sesji
+  resave: false, // Unikaj zapisywania sesji, jeśli nie zmieniły się dane
+  saveUninitialized: false, // Nie zapisuj pustych sesji
   cookie: {
-    secure: false, // Ustaw `true` dla HTTPS
-    sameSite: 'lax', // Dla środowiska produkcyjnego ustaw 'none'
-  }
+      maxAge: 30 * 60 * 1000, // Czas życia ciasteczka w ms (30 minut)
+      secure: false, // Ustaw `true` dla HTTPS
+      sameSite: 'lax', // `lax` dla ochrony przed atakami CSRF
+  },
+  // Funkcje serializacji i deserializacji
+  serialize: (session) => JSON.stringify(session), // Serializacja do JSON
+  unserialize: (json) => JSON.parse(json)          // Deserializacja z JSON
 }));
+
+//app.use(session({
+//  secret: config['server']['key'],
+//  resave: false,
+//  saveUninitialized: true,
+//  cookie: {
+//    secure: false, // Ustaw `true` dla HTTPS
+//    sameSite: 'lax', // Dla środowiska produkcyjnego ustaw 'none'
+//  }
+//}));
 
 app.get('/', (req, res) => {
   res.send(`

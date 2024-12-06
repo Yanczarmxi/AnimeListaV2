169c84682c05b46b1a00d78ddf47381bd523e974
Yancarmxi <yanczi@outlook.com>
2024-12-06 20:08:23 +0100

    Jakieś czary mary z serializacją danych do wyśłaniana frontend

--- server/src/extraction.js
+++ server/src/extraction.js
@@ -1,29 +1,81 @@
-async function GroupsDataTable(){
-    //const animes = await GetAnime();
-    const GetGroups = require('./database/groups');
+const GetAnimes = require('./database/anime')
+const GetGroups = require('./database/groups');
 
-    const groups = await GetGroups(1);
+function SegregatedAnimeToGroup(groups, animes){
     var data = [];
-
-    console.log(groups);
-
-    console.log(groups.length);
-    
+    var tmp = [];
     for(var i=0; i < groups.length; i++){
+        for(var j=0; j < animes.length; j++){
+            if(animes[j].st_group == groups[i].gr_id){
+                tmp.push({
+                    id:       animes[j].an_id,
+                    title:    animes[j].an_title,
+                    date:     animes[j].an_date,
+                    url:      animes[j].an_url,
+                    episodes: animes[j].an_episodes,
+                    img:      animes[j].an_miniature,
+                    fav: {
+                        status:  animes[j].fv_state,
+                        episode: animes[j].fv_episode
+                    }
+                });
+            }
+        }
+    
         data.push({
             gid: groups[i].gr_id,
             gtitle: groups[i].gr_title,
-            anime: []
+            anime: tmp
         });
+    
+        tmp = [];
     }
-
-    console.log(data);
 
     return data;
 }
 
-async function AnimeDataTable(params) {
+function SegregateAnimesTootchers(animes){
+    var tmp = [];
+    for(var i=0; i < animes.length; i++){
+        if(animes.an_group == null){
+            tmp.push({
+                id:       animes[i].an_id,
+                title:    animes[i].an_title,
+                date:     animes[i].an_date,
+                url:      animes[i].an_url,
+                episodes: animes[i].an_episodes,
+                img:      animes[i].an_miniature,
+                fav: {
+                    status:  animes[i].fv_state,
+                    episode: animes[i].fv_episode
+                }
+            });
+        }
+    }
     
+    return tmp;
 }
 
-module.exports = GroupsDataTable;
+function SearchAnimeIndex(animes){
+    var tmp = [];
+
+    for(var i=0; i < animes.length; i++){
+        tmp.push({
+            id: animes.an_id,
+            title: animes.an_title
+        });
+    }
+
+    return tmp;
+}
+
+const groups = await GetGroups(1);
+const animes = await GetAnimes(1);
+
+var data = [{
+    segregated: SegregatedAnimeToGroup(groups, animes),
+    others: SegregateAnimesTootchers(animes),
+    search: SearchAnimeIndex(animes)
+}];
+
+module.exports = data;

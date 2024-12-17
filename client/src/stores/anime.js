d4b44512b51dcc5be6e0bf94899a5f9669d1cec0
Yancarmxi <yanczi@outlook.com>
2024-12-17 17:06:15 +0100

    qwerty

--- client/src/stores/anime.js
+++ client/src/stores/anime.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 
 export const useAnimeStore = defineStore('Anime', {
     state: () => ({
-        data: {},
         search: {},
         group: {},
-        
+        animedata: [],
+        isLoaded: false,
 
         //adres do validacji
         serverUrl: `${process.env.VUE_APP_SERVER}`
@@ -16,19 +16,59 @@ export const useAnimeStore = defineStore('Anime', {
             try{
                 const response = await axios.get('/anime/result', {withCredentials: true});
 
-                this.data = {
-                    segregated: response.data.segregated,
-                    others: response.data.others
-                };
+                this.PrepareAnimeData(response.data.segregated, response.data.others);
 
                 this.search = response.data.search;
                 this.group = response.data.group;
 
                 console.log(response); //DEBUG
+                this.isLoaded = true;
+
+                return true;
             }
             catch(e){
-                console.error('Nie udało się pobrać danych')
+                console.error('Nie udało się pobrać danych');
+                return false;
+            }
+        },
+
+        PrepareAnimeData(grd, ohd){
+            this.animedata = grd;
+            this.animedata.push({
+                gid: 0,
+                gtitle: 'Pozostałe',
+                anime: ohd
+            });
+        },
+
+        GetAnimeData(){
+            if(this.isLoaded){
+                return {
+                    data: this.animedata,
+                    search: this.search,
+                    group: this.group          
+                };
+            }
+            else{
+                if(GetAnime()){
+                    return {
+                        data: this.animedata,
+                        search: this.search,
+                        group: this.group          
+                    };
+                }
+                return {mess: 'Nie udało się pobrać animu :c'};
             }
         }
-    }
+    },
+
+    persist: {
+        enabled: true,
+        strategies: [
+          {
+            key: 'store_anime',
+            storage: localStorage,
+          },
+        ],
+    },
 });
